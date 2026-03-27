dc075202b7d5e88cf107c3379ac02aa11b2f5f6a
Nicholas Gerber <nzg0069@auburn.edu>
2026-03-27 11:57:30 -0400

    Finished following video #2

--- Favorites/ContentView.swift
+++ Favorites/ContentView.swift
@@ -19,13 +19,13 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selection) {
-            Tab("Home", systemImage: "square.grid.2x2", value: .home) {
+            Tab("Home", systemImage: "square.grid.2x2", value: .home){
                 HomeView()
             }
             Tab("Favorites", systemImage: "star.fill", value: .favorites) {
                 FavoritesView()
             }
-            Tab("Settings", systemImage: "gearshape", value: .settings) {
+            Tab("Settings", systemImage: "gearshpae", value: .settings) {
                 SettingsView()
             }
         }
---
+++ Favorites/Views/HobbyRowView.swift
@@ -0,0 +1,36 @@
+//
+//  HobbyRowView.swift
+//  Favorites
+//
+//  Created by Nick.Gerber.2 on 2026-03-27.
+//
+
+import SwiftUI
+
+struct HobbyRowView: View {
+    
+    let hobby: HobbyModel
+    @EnvironmentObject private var favorites: FavoritesViewModel
+    var body: some View {
+        HStack {
+            Text(hobby.hobbyIcon)
+                .font(.title2)
+            Text(hobby.hobbyName)
+                .font(.body)
+            
+            Spacer()
+            
+            Button(action: {
+                print("Tapped for \(hobby.hobbyName)")
+            }) {
+                Image(systemName: hobby.isFavorite ? "heart.fill" : "heart")
+                    .foregroundStyle(hobby.isFavorite ? .red : .white)
+            }
+        }
+        .padding(.vertical, 4)
+    }
+}
+
+#Preview {
+    HobbyRowView(hobby: HobbyModel(id: 1, hobbyName: "Hiking", hobbyIcon: "🥾"))
+}
